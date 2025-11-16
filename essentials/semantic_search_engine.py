85c9e3fc7c3d24b118a8c7c2f67e11fbe35d0dec
OsamaIssa95 <osama.issa.093@gmail.com>
2025-11-16 16:38:39 +0300

    --add pymupdf4llm to output text

--- essentials/semantic_search_engine.py
+++ essentials/semantic_search_engine.py
@@ -2,6 +2,8 @@ from langchain_core.documents import Document
 from langchain_community.document_loaders import FileSystemBlobLoader, PyPDFLoader
 from langchain_community.document_loaders.parsers import PyPDFParser, TesseractBlobParser
 from langchain_community.document_loaders.generic import GenericLoader
+import pymupdf4llm
+import pathlib
 
 #document structure
 """ documents = [
@@ -15,20 +17,35 @@ from langchain_community.document_loaders.generic import GenericLoader
     ),
 ] """
 
-def pdfloader(path: str) ->list[Document]:
-    loader = GenericLoader(   
-    blob_loader = FileSystemBlobLoader(
-        path=path,
-        glob="*pdf",
-    ),
-    blob_parser=PyPDFParser(mode="single",
-                            pages_delimiter="--------end--------",
-                            images_inner_format="markdown-img",
-                            images_parser=TesseractBlobParser,
-                            extraction_mode="layout",
-                            ),
-    )
-    return loader.load()
 
-docs = pdfloader("static/PDF")
+def pdfloader(path: str) -> list[Document]:
+    """ This function loads a PDF file from the given path and returns a list of Document objects.
+    Args:
+        path: The path to the PDF file.
+    Returns:
+        A list of Document objects.
+    """
+    loader = GenericLoader(   
+        blob_loader=FileSystemBlobLoader(
+            path=path,
+            glob="*.pdf",
+            ),
+        blob_parser=PyPDFParser(
+            mode="single",
+            pages_delimiter="--------end--------",
+            images_inner_format="markdown-img",
+            images_parser=TesseractBlobParser,
+            extraction_mode="layout",
+            ),
+    )
+    docs = loader.load()
+    return docs
+
+
+docs = pdfloader("static/PDF") 
 print(docs[0].page_content)
+
+
+""" md_text = pymupdf4llm.to_markdown("./static/PDF/CV.pdf")
+pathlib.Path("output.md").write_bytes(md_text.encode())
+print(md_text) """
